e1a0964666424d73842ff6b6dfa0ce5c40313255
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-02-20 21:38:44 +0600

    20/02/2025

---
+++ bin/lib/operators/arithmetic_operators.dart
@@ -0,0 +1,10 @@
+main(){
+  var lincon = 19;
+  var shoun = 21;
+
+  print ('lincon + shoun = ${lincon + shoun}');
+  print ('lincon + shoun = ${lincon - shoun}');
+  print ('lincon + shoun = ${lincon * shoun}');
+  print ('lincon + shoun = ${lincon / shoun}');
+  print ('lincon + shoun = ${lincon % shoun}');
+}
