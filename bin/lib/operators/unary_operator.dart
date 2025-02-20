6fbff672c50ff67561832f6e9462075f712e634c
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-02-21 01:52:21 +0600

    21/02/2025

---
+++ bin/lib/operators/unary_operator.dart
@@ -0,0 +1,23 @@
+main(){
+
+  var lincon = 19;
+  print(lincon++);
+  print(lincon++);
+  print(lincon++);
+
+  var shoun = 35;
+  print(++shoun);
+  print(++shoun);
+  print(++shoun);
+
+  var bipul = 50;
+  print(bipul--);
+  print(bipul--);
+  print(bipul--);
+
+  var tripti = 90;
+  print(--tripti);
+  print(--tripti);
+  print(--tripti);
+
+}
