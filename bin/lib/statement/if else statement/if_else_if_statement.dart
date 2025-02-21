2289f3728d27c0c56d73932a55efa051a1bb788d
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-02-22 02:42:38 +0600

    22/02/2025

---
+++ bin/lib/statement/if else statement/if_else_if_statement.dart
@@ -0,0 +1,10 @@
+void main(){
+  var lincon = 19;
+  var shoun = 21;
+
+  if (lincon > shoun){
+    print('lincon is greater then shoun');
+  }else{
+    print('shoun is greater then lincon');
+  }
+}
