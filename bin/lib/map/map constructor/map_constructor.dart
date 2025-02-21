8f0c045be4776b7120ec7633f214b9d61373cde8
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-02-22 01:02:59 +0600

    21/02/2025

---
+++ bin/lib/map/map constructor/map_constructor.dart
@@ -0,0 +1,9 @@
+void main(){
+  var student = new Map();
+  student ['Name'] = 'Lincon';
+  student ['Age'] = '19';
+  student ['Roll'] = '998845';
+  student ['Department'] = 'CMT';
+
+  print(student);
+}
