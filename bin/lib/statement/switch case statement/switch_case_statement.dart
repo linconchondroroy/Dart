9189dba34c1306688f00711a991731afd75ad70c
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-02-22 11:43:36 +0600

    22/02/2025

---
+++ bin/lib/statement/switch case statement/switch_case_statement.dart
@@ -0,0 +1,20 @@
+void nain(){
+  var marks =66;
+
+  switch(marks){
+    case 80:
+      print('A+');
+      break;
+
+    case 70:
+      print('A');
+      break;
+
+    case 60:
+      print('A-');
+      break;
+
+    default:
+      print('Result Not Found');
+  }
+}
