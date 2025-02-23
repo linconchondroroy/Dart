bb59a9c771631458d6560e8f5f7e5a4fee1ad65c
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-02-23 19:44:57 +0600

    23/02/2025

---
+++ bin/lib/funcation/funcation_call/funcation_call.dart
@@ -0,0 +1,8 @@
+myFuncation(){
+  var lincon = 20;
+  var shoun = 30;
+  print(lincon+shoun);
+}
+void main(){
+  myFuncation();
+}
---
+++ bin/lib/loop/do while loop/do_while_loop.dart
@@ -0,0 +1,7 @@
+void main(){
+  var i=0;
+  do{
+    print(i);
+    i=i+1;
+  }while(i<100);
+}
