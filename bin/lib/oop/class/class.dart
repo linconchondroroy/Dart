82ae398cdf2acbf858beee695f6f3ab327e465c2
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-02-25 23:40:02 +0600

    25/02/2025

--- bin/lib/oop/class/class.dart
+++ bin/lib/oop/class/class.dart
@@ -3,6 +3,15 @@ class MyClass{
   var Alphabet =['A','B','C'];
   addTowNumber(int x, int y){
     print(x+y);
-
   }
+  addThreeNumber(int x, int y, int z){
+    print(x+y+z);
+  }
+}
+void main(){
+  var obj = MyClass();
+  obj.addTowNumber(10, 20);
+  obj.addThreeNumber(25, 15, 20);
+  print(obj.MyName);
+  print(obj.Alphabet[0]);
 }
